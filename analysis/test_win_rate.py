f1ec4aa8d725a917807d9c9ce205827d3ce1912d
Max Xu <max.zhuofan.xu@gmail.com>
2025-02-21 12:13:45 +0100

    add script for hpn 6h8z

--- analysis/test_win_rate.py
+++ analysis/test_win_rate.py
@@ -6,10 +6,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 plot_path = "learning_rate.png"
+window = 5
 
 # Currently allow one exp of each map / agent at a time
 data_paths = [
-
+    '/Users/zhuofan.xu/Downloads/pymarl_results/8m_vs_9m/spe_qmix/2',
+    '/Users/zhuofan.xu/Downloads/pymarl_results/8m_vs_9m/sattpe_qmix/1',
+    "/Users/zhuofan.xu/Downloads/pymarl_results/8m_vs_9m/hpn_qmix/6",
+    '/Users/zhuofan.xu/Downloads/pymarl_results/8m_vs_9m/attrpe_qmix/4',
     'results/sacred/8m_vs_9m/qmix/1',
 
 ]
@@ -29,6 +33,7 @@ for data_path in data_paths:
             all_data.append(config_data | win_rate_data)
 
 df = pd.DataFrame(all_data)
+df["wind_win_rate"] = df["test_win_rate"].rolling(window, closed="both", center=True).mean()
 
 
 # test_won_rate = data["test_battle_won_mean"]
@@ -36,6 +41,7 @@ df = pd.DataFrame(all_data)
 plt.figure(figsize=(12, 3))
 
 win_rate_plot = sns.relplot(data=df, x="T", y="test_win_rate", hue="agent", palette="tab10", col="map", kind="line", aspect=3)
+# win_rate_plot = sns.relplot(data=df, x="T", y="wind_win_rate", hue="agent", palette="tab10", col="map", kind="line", aspect=3)
 
 plt.savefig(plot_path, dpi=100, bbox_inches="tight")
 
